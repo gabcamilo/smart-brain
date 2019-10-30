88cee9bc4ed4a451d71c9748c3c411acb4b1f576
Gabriela Carvalho Camilo <gabriela.camilo@pca.com.br>
2019-10-30 16:17:09 -0300

    added components

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Rank = () =>{
+	return(
+		<div>
+			<div className="white f3">
+			{"Jane Doe, your current rank is..."}
+			</div>
+			<div className="white f1">
+			{"#8"}
+			</div>
+		</div>
+	)
+}
+
+export default Rank;
+
